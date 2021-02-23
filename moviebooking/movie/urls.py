57b266c2c4d8ade543dd82683d7569215e56aa95
Preshly <mca.1911@unigoa.ac.in>
2021-02-23 15:21:55 +0530

    admin dashboard changes

--- moviebooking/movie/urls.py
+++ moviebooking/movie/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views 
 
 urlpatterns = [
-    path('',views.index),
-]
+    path('',views.index, name='movieIndex'),
+] 
+#+ static(settings.STATIC_URL, documnet_root=settings.STATICFILES_DIRS)
+
+admin.site.site_header = 'Movie Booking'
+admin.site.site_title = 'Movie Booking'
+admin.site.index_title = 'Welocome to the Admin page'
--- moviebooking/moviebooking/urls.py
+++ moviebooking/moviebooking/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movie/',include('movie.urls'))
+    path('movie/',include('movie.urls')),
+    path('', views.index),
 ]
