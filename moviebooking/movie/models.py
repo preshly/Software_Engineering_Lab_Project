a03cc187c9d6eb39a6129d74ab9aef077cdab185
Shawn11a <dshawn1109@gmail.com>
2021-03-09 23:53:01 +0530

    Tables created - sprint2

--- moviebooking/movie/models.py
+++ moviebooking/movie/models.py
@@ -1,11 +1,31 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator
 
 class Movie(models.Model):
     movie_id = models.AutoField
     movie_name = models.CharField(max_length=50)
     movie_language = models.CharField(max_length=30)
     movie_duration = models.IntegerField()
-
+    movie_description = models.CharField(max_length=256, default="")
+    
+    image = models.ImageField(upload_to='media/images', default="")
     def __str__(self):
         return self.movie_name
+
+class Genre(models.Model):
+    genre_id = models.AutoField
+    genre_name = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.genre_name
+
+
+class Customer(models.Model):
+    cust_id = models.AutoField
+    email = models.EmailField()
+    username = models.CharField(max_length=10, validators=[MinLengthValidator(10)])
+    password = models.CharField(max_length=8, validators=[MinLengthValidator(8)])
+
+    def __str__(self):
+        return self.username
